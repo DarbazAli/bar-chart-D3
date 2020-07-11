ebb989a57f8d09565b9755bc443ceb67ca01bbbb
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-11 18:44:38 +0300

    refactor tooltip format

--- App.js
+++ App.js
@@ -49,6 +49,25 @@ const parseTime = d3
 const formatTime = d3.timeFormat("%Y-%m-%d");
 
 
+const drawTooltip = ( d, tooltip) => {
+    tooltip 
+
+        .style("opacity", 1)
+
+        .style("left", `${d3.event.layerX - 100}px`)
+        .style("top", `${d3.event.layerY - 40}px`)
+
+        .attr("data-date", formatTime(d[0]))
+        .text(() => {
+
+            let year = d[0].getFullYear();
+            let quarter = (d[0].getMonth() == 0) ? "Q1" : (d[0].getMonth() == 3) ? "Q2" : (d[0].getMonth() == 6) ? "Q3" : "Q4";
+
+            return `${year} ${quarter} $${d[1]} Billion`;
+        });
+}
+
+
 
 const URL = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
@@ -68,6 +87,7 @@ function drawBarChart(data) {
         d[1] = +d[1];
     });
 
+
     // DOMAIN
     xScale
         .domain(d3.extent(data, d => d[0]));
@@ -82,6 +102,8 @@ function drawBarChart(data) {
                     .axisBottom(xScale);
     const yAxis = d3
                     .axisLeft(yScale);
+
+
     canvasContents
         .append("g")
         .attr("id", "x-axis")
@@ -105,23 +127,8 @@ function drawBarChart(data) {
         .enter()
         .append("rect")
 
-        .on("mouseenter", (d) => {
-            tooltip 
+        .on("mouseenter", (d) => drawTooltip(d, tooltip))
 
-                .style("opacity", 1)
-
-                .style("left", `${d3.event.layerX - 100}px`)
-                .style("top", `${d3.event.layerY - 40}px`)
-
-                .attr("data-date", formatTime(d[0]))
-                .text(() => {
-
-                    let year = d[0].getFullYear();
-                    let quarter = (d[0].getMonth() == 0) ? "Q1" : (d[0].getMonth() == 3) ? "Q2" : (d[0].getMonth() == 6) ? "Q3" : "Q4";
-
-                    return `${year} ${quarter} $${d[1]} Billion`;
-                });
-        })
         .on("mouseout", () => {
             tooltip
                 .style("opacity", 0);
@@ -131,11 +138,13 @@ function drawBarChart(data) {
         .attr("data-gdp", (d) => d[1])
 
         .attr("x", (d, i) => (width/ data.length) * i)
-
-        .attr("width", (width/ data.length))
-
         .attr("y", (d) => yScale(d[1]))
 
+        .attr("width", (width/ data.length))
         .attr("height", (d) => height - yScale(d[1]))
+
         .attr("class", "bar");
 }
+
+
+
