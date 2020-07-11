6264e2d92e05525862d3d7a1fab162248dc45912
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-11 16:53:55 +0300

    add props and functions

--- App.js
+++ App.js
@@ -6,4 +6,50 @@ const container = d3.select(".container");
 container
     .append("h1")
     .attr("id", "title")
-    .text("Gross Domestic Product 📈");
+    .text("Gross Domestic Product");
+
+
+// MARGINS
+const margin = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 50
+}
+
+// CANVAS HIGHT AND WIDTH
+const width = 800 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+
+
+// CANVAS
+const canvas = container.append('svg')
+.attr("viewBox", `0 0 ${width + margin.left + margin.right}  ${height + margin.top + margin.bottom}`);
+
+
+
+const axisGroups = canvas.append('g')
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+
+
+// SCALES
+
+const xScale = d3
+                .scaleTime()
+                .range([0, width]);
+
+
+const yScale = d3
+                .scaleLinear()
+                .range([height, 0]);
+
+
+const parseTime = d3
+                    .timeParse("%Y-%m-%d");
+
+
+const formatTime = d3.timeFormat("%Y-%m-%d");
+
