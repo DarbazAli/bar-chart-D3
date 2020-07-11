814a89af443e46adddfb134a7e0eb2e67cabbfd0
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-11 17:18:39 +0300

    Plot bar chart

--- App.js
+++ App.js
@@ -56,12 +56,106 @@ const formatTime = d3.timeFormat("%Y-%m-%d");
 
 
 
-const URL = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+const apiURL = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-fetch( URL )
+fetch( apiURL )
 .then( res => res.json())
 .then( json => {
     const data  = json["data"];
     // console.log(data);
+    drawBarChart(data)
+});
+
+
+
+
+const drawBarChart = data => {
+
+    data.forEach( d => {
+        d[0] = parseTime(d[0]);
+        d[1] = +d[1]
+    });
+
+
+    // DOMAIN
+    xScale
+        .domain(d3.extent(data, d => d[0]));
+        
+    yScale
+        .domain(d3.extent(data, d => d[1]))
+        .nice();
+
+
+     // AXES 
+    // initialize the axes based on the scales
+    const xAxis = d3
+                    .axisBottom(xScale);
+    const yAxis = d3
+                    .axisLeft(yScale);
+
+
+    // X Axis bar
+    axisGroups
+            .append('g')
+            .attr('id', 'x-axis')
+            .attr('transform', `translate(0, ${height})`)
+            .call(xAxis)
+    // Y Axis Bar
+    axisGroups
+            .append('g')
+            .attr('id', 'y-axis')
+            .call(yAxis)
+
+
+    // TOOLTIP
+    // include a tooltip through a div element
+    const tooltip = container
+                        .append("div")
+                        .attr("id", "tooltip");
+
+
+    // PLOT CHART
+    canvas.selectAll('rect')
+            .data(data)
+            .enter()
+            .append('rect')
+
+            .on("mouseenter", (d) => {
+                tooltip 
+                    // alter the opacity to make the tooltip visible
+                    .style("opacity", 1)
+                    // position the tooltip close to the cursor, using the d3.event object
+                    // console.log() this object to establish which properties are needed
+                    .style("left", `${d3.event.layerX - 150}px`)
+                    .style("top", `${d3.event.layerY - 80}px`)
+                    // include a data-date property which describes the date of the connected rectangle element
+                    // date formatted through the defined format function
+                    .attr("data-date", formatTime(d[0]))
+                    .text(() => {
+                        // d[0], as it is processed through the parse function, represents an instance of the date object
+                        // getFullYear() allows to retrieve the four-digit year 
+                        let year = d[0].getFullYear();
+                        let quarter = (d[0].getMonth() == 0) ? "Q1" : (d[0].getMonth() == 3) ? "Q2" : (d[0].getMonth() == 6) ? "Q3" : "Q4";
     
-})
+                        return `${year} ${quarter} ${d[1]}`;
+                    });
+            })
+            .on("mouseout", () => {
+                tooltip
+                    .style("opacity", 0);
+            })
+
+            // parse data to data holders
+            .attr('data-date', d => formatTime(d[0]))
+            .attr('data-gdp', d => d[1])
+
+            .attr('x', (d, i) => (width / data.length) * i )
+            .attr('y', d => yScale(d[1]))
+
+            .attr('width', width / data.length )
+            .attr('height', d => height - yScale(d[1]))
+
+            .attr('class', 'bar')
+
+}
+
